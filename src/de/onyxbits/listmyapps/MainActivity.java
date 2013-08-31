655de118319d37c3a4f4244ddacfac429167269a
patrick <github@onyxbits.de>
2013-09-01 00:55:00 +0200

    Cleanup

--- src/de/onyxbits/listmyapps/MainActivity.java
+++ src/de/onyxbits/listmyapps/MainActivity.java
@@ -11,7 +11,6 @@ import android.text.ClipboardManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
