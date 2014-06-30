3f8a1588ff22098bda9e9433794063f4fdde394e
patrick <github@onyxbits.de>
2014-07-01 01:36:02 +0200

    Added assistant for inserting variables into templates.

--- src/de/onyxbits/listmyapps/TemplateEditorActivity.java
+++ src/de/onyxbits/listmyapps/TemplateEditorActivity.java
@@ -3,12 +3,16 @@ package de.onyxbits.listmyapps;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class TemplateEditorActivity extends Activity {
+public class TemplateEditorActivity extends Activity implements OnClickListener {
 
 	/**
 	 * For passing in an format id via intents.
@@ -53,8 +57,7 @@ public class TemplateEditorActivity extends Activity {
 					finish();
 				}
 				else {
-					Toast.makeText(this, R.string.msg_error_incomplete, Toast.LENGTH_SHORT)
-							.show();
+					Toast.makeText(this, R.string.msg_error_incomplete, Toast.LENGTH_SHORT).show();
 				}
 				return true;
 			}
@@ -63,12 +66,34 @@ public class TemplateEditorActivity extends Activity {
 				finish();
 				return true;
 			}
+			case R.id.variable: {
+				showVariableSelector();
+				return true;
+			}
 			case R.id.item_help: {
 				Uri uri = Uri.parse(getString(R.string.url_help));
-				MainActivity.openUri(this,uri);
+				MainActivity.openUri(this, uri);
 			}
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private void showVariableSelector() {
+		View focus = getWindow().getCurrentFocus();
+		if (focus.getId() == R.id.tmpl_name) {
+			Toast.makeText(this, R.string.msg_name_does_not_support_variables, Toast.LENGTH_SHORT).show();
+			return;
+		}
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(R.string.title_variable_to_insert);
+		if (focus.getId() == R.id.tmpl_item) {
+			builder.setItems(R.array.templatebodyvars, this);
+		}
+		if (focus.getId() == R.id.tmpl_footer || focus.getId() == R.id.tmpl_header) {
+			builder.setItems(R.array.templateheaderfootervars, this);
+		}
+		builder.show();
 	}
 
 	/**
@@ -77,16 +102,14 @@ public class TemplateEditorActivity extends Activity {
 	 * @return true if the model can be saved to the database.
 	 */
 	private boolean toModel() {
-		editing.formatName = MainActivity
-				.noNull(((EditText) findViewById(R.id.tmpl_name)).getText().toString());
-		editing.header = MainActivity
-				.noNull(((EditText) findViewById(R.id.tmpl_header)).getText()
-						.toString());
-		editing.item = MainActivity
-				.noNull(((EditText) findViewById(R.id.tmpl_item)).getText().toString());
-		editing.footer = MainActivity
-				.noNull(((EditText) findViewById(R.id.tmpl_footer)).getText()
-						.toString());
+		editing.formatName = MainActivity.noNull(((EditText) findViewById(R.id.tmpl_name)).getText()
+				.toString());
+		editing.header = MainActivity.noNull(((EditText) findViewById(R.id.tmpl_header)).getText()
+				.toString());
+		editing.item = MainActivity.noNull(((EditText) findViewById(R.id.tmpl_item)).getText()
+				.toString());
+		editing.footer = MainActivity.noNull(((EditText) findViewById(R.id.tmpl_footer)).getText()
+				.toString());
 
 		if (!editing.formatName.equals("") && !editing.item.equals("")) {
 			return true;
@@ -94,4 +117,22 @@ public class TemplateEditorActivity extends Activity {
 		return false;
 	}
 
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		View focus = getWindow().getCurrentFocus();
+		if (focus.getId() == R.id.tmpl_item) {
+			EditText et = (EditText) focus;
+			int start = Math.max(et.getSelectionStart(), 0);
+			int end = Math.max(et.getSelectionEnd(), 0);
+			String txt = getResources().getStringArray(R.array.templatebodyvars)[which];
+			et.getText().replace(Math.min(start, end), Math.max(start, end), txt, 0, txt.length());
+		}
+		if (focus.getId() == R.id.tmpl_footer || focus.getId() == R.id.tmpl_header) {
+			EditText et = (EditText) focus;
+			int start = Math.max(et.getSelectionStart(), 0);
+			int end = Math.max(et.getSelectionEnd(), 0);
+			String txt = getResources().getStringArray(R.array.templateheaderfootervars)[which];
+			et.getText().replace(Math.min(start, end), Math.max(start, end), txt, 0, txt.length());
+		}
+	}
 }
